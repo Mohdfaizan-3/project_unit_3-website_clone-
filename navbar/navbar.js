a12aa6943fab830e396237df53d6610784e1909c
Mohd Faizan <faizan.mohd09@gmail.com>
2022-06-13 21:39:23 +0530

    done header and footer

---
+++ navbar/navbar.js
@@ -0,0 +1,113 @@
+function header(){
+    return `
+    
+    
+    
+        <div id="category" class="p-2  btnbottom  flex-grow-2 ml-0 mr-5 w-75  fs-3 bg-dark text-light fw-bolder">
+            Category</div>
+        <div class="p-2 btnbottom flex-grow-2 w-75 mx-5 fs-3 bg-warning fw-bolder ">COUPON</div>
+        <div class="p-2  btnbottom  flex-grow-2 w-75 mx-5 fs-3 bg-warning fw-bolder">SUPERDEALS</div>
+        <div class="p-2  btnbottom  flex-grow-2 w-75 mx-5 fs-3 bg-warning fw-bolder">APP_ONLY</div>
+        <div class="p-1  btnbottom  flex-grow-2  mx-4 fs-3 bg-warning fw-bolder">NEW_ARRIVAL </div>
+    
+    
+    
+    `
+}
+
+
+function footer(){
+    return `<div class="container" id="footerStart">
+
+
+    <div>
+        <span class="material-symbols-outlined">
+            home
+        </span>
+        <h4>HIGH QUALITY SELECTION</h4>
+        <p class="text-secondary">total product quality control for peace of mind</p>
+    </div>
+    <div>
+        <span class="material-symbols-outlined">
+            thumb_up
+        </span>
+        <h4>AFFORDABLE PRICES</h4>
+        <p class="text-secondary">Factory direct prices for maximum savings</p>
+    </div>
+    <div>
+        <span class="material-symbols-outlined">
+            local_shipping
+        </span>
+        
+        <h4>Express Shipping</h4>
+        <p class="text-secondary">Fast, reliable delivery from global warehouses</p>
+    </div>
+    <div>
+        <span class="material-symbols-outlined">
+            star_half
+        </span>
+        <h4>Worry-free After-sales</h4>
+        <p class="text-secondary">Instant access to professional support</p>
+    </div>
+</div>
+
+
+<div class="footer container mt-5">
+    <div>
+        <ul class="size">
+            <li>
+                <h4>About R+F</h4>
+            </li>
+            <li>Press Room</li>
+            <li>Company Info</li>
+            <li>Executive Team</li>
+            <li>Careers</li>
+            <li>R+F Recycling Program</li>
+            <li>Blog</li>
+        </ul>
+    </div>
+    <div>
+        <ul class="size">
+            <li>
+                <h4>help</h4>
+            </li>
+            <li>Product & Help Center</li>
+            <li>Shipping</li>
+            <li>Satisfaction Guarantee</li>
+            <li>Promotional&nbsp;erms&nbsp;&&nbsp;Conditions</li>
+            <li>R+F FAQS</li>
+            <li>Klarna</li>
+            <li>Site Map</li>
+        </ul>
+    </div>
+    <div></div>
+    <div>
+        <h3 class="title2">Let's get Social</h3>
+        <div class="social">
+            <i class="fa-brands fa-facebook-f"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-youtube"></i>
+        </div>
+
+    </div>
+</div>
+<br>
+<!-- ============footer end ============-->
+<!-- =============copyright start ===== -->
+<div class="cpoyright container">
+    <div><span>© 2022 Rodan & FieldsPrivacy Policy</span></div>
+    <div><span>Terms & Conditions</span></div>
+    <div><span>Accessibility</span></div>
+    <div><span>California Supply Chains Act</span></div>
+    <div>DSA Code of Ethics</div>
+    <div><span>Disclaimer</span></div>
+    <div>Preference Center</div>
+</div>
+
+
+`;
+}
+
+
+export {header,footer}
